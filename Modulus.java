7060ca8011a7fa34c396ed06f6ac048f73b6df4a
SaiKiran-Reddy1114 <119839640+SaiKiran-Reddy1114@users.noreply.github.com>
2023-08-05 12:04:21 +0530

    import java.util.Scanner; public class Modulus{     public static void main(String[] args)     {         Scanner sn= new Scanner(System.in);         int a,b,c;         a=sn.nextInt();         b=sn.nextInt();         System.out.println(a%b);     } }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Modulus{
+    public static void main(String[] args)
+    {
+        Scanner sn= new Scanner(System.in);
+        int a,b,c;
+        a=sn.nextInt();
+        b=sn.nextInt();
+        System.out.println(a%b);
+    }
+}
