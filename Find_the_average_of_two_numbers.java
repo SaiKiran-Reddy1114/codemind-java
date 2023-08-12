d83a9c583711b8fff829310979a3b48de37813d1
SaiKiran-Reddy1114 <119839640+SaiKiran-Reddy1114@users.noreply.github.com>
2023-08-12 10:00:07 +0530

    import java.util.*; public class Kiran{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         double a,b,c;         a=n.nextDouble();         b=n.nextDouble();         c=(a+b)/2;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Kiran{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        double a,b,c;
+        a=n.nextDouble();
+        b=n.nextDouble();
+        c=(a+b)/2;
+        System.out.printf("%.4f",c);
+    }
+}
