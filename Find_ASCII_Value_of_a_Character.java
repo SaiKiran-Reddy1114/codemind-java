18d4fd5b17534587acbd22f813bc8806b8b740de
SaiKiran-Reddy1114 <119839640+SaiKiran-Reddy1114@users.noreply.github.com>
2023-10-04 14:22:41 +0530

    import java.util.*; public class Charm{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String c=sc.next();         char ch=c.charAt(0);         int n;         n=ch;         System.out.print(n);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Charm{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String c=sc.next();
+        char ch=c.charAt(0);
+        int n;
+        n=ch;
+        System.out.print(n);
+    }
+}
