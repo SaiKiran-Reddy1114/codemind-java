018bb3fa3b0bbc80b8e2de37ec5f3570be936945
SaiKiran-Reddy1114 <119839640+SaiKiran-Reddy1114@users.noreply.github.com>
2023-10-20 11:26:24 +0530

    import java.util.*; public class Charm{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int k=sc.nextInt();         if(k%2==0)         {             System.out.print("Even");         }         else         {             System.out.print("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Charm{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        if(k%2==0)
+        {
+            System.out.print("Even");
+        }
+        else
+        {
+            System.out.print("Odd");
+        }
+    }
+}
